ed38cfacd6c297e38bef81740997b6a345ac37ba
AlDAan17 <kototi17@gmail.com>
2020-11-21 01:24:57 +0300

    added action types

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,43 +1,53 @@
 import {getIdFromAPI, getTicketsFromAPI} from "../services/ticket-service";
+import {
+    SHOW_ALL,
+    SHOW_WITHOUT_TRANSFERS,
+    SHOW_WITH_ONE_TRANSFER,
+    SHOW_WITH_TWO_TRANSFERS,
+    SHOW_WITH_THREE_TRANSFERS,
+    SELECT_TAB,
+    TICKETS_RECEIVED,
+    TICKETS_NOT_RECEIVED,
+} from './action-types';
 
 export const showAll = (isActive) => ({
-    type: 'SHOW_ALL',
+    type: SHOW_ALL,
     active: isActive,
 });
 
 export const showNothing = (isActive) => ({
-    type: 'SHOW_WITHOUT_TRANSFERS',
+    type: SHOW_WITHOUT_TRANSFERS,
     active: isActive,
 });
 
 export const showOne = (isActive) => ({
-    type: 'SHOW_WITH_ONE_TRANSFER',
+    type: SHOW_WITH_ONE_TRANSFER,
     active: isActive,
 });
 
 export const showTwo = (isActive) => ({
-    type: 'SHOW_WITH_TWO_TRANSFERS',
+    type: SHOW_WITH_TWO_TRANSFERS,
     active: isActive,
 });
 
 export const showThree = (isActive) => ({
-    type: 'SHOW_WITH_THREE_TRANSFERS',
+    type: SHOW_WITH_THREE_TRANSFERS,
     active: isActive,
 });
 
 export const selectTab = (selected) => ({
-    type: 'SELECT_TAB',
+    type: SELECT_TAB,
     selected,
 });
 
 const ticketsReceived = (tickets, stop) => ({
-    type: 'TICKETS_RECEIVED',
+    type: TICKETS_RECEIVED,
     tickets,
     stop,
 });
 
 const ticketsNotReceived = () => ({
-    type: 'TICKETS_NOT_RECEIVED',
+    type: TICKETS_NOT_RECEIVED,
 });
 
 export const asyncGetTickets = () => {
